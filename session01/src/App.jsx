7506c90a7b1e366faa094bdf1896467d0e0a25d2
Times <alfred.times@yahoo.fr>
2025-12-02 12:58:08 -0500

    TD02: Afficher la date et l'heure du jour

--- session01/src/App.jsx
+++ session01/src/App.jsx
@@ -17,9 +17,16 @@ function Header() {
 }
 
 function MainContent() {
+  const now = new Date();
+  const jour = now.getDate().toString().padStart(2, "0");   // 01–31
+  const mois = now.toLocaleString("fr-FR", { month: "long" }); // janvier, février…
+  const annee = now.getFullYear();
+  const heure = now.getHours().toString().padStart(2, "0");
+  const minute = now.getMinutes().toString().padStart(2, "0");
+  const seconde = now.getSeconds().toString().padStart(2, "0");
   return (
     <main>
-      Ici, nous afficherons des informations interessantes :)
+       Bonjour, on est le {jour}, {mois}, {annee} et il est {heure}:{minute}:{seconde}
     </main>
   );
 }
