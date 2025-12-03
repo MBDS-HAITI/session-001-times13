7c4d08ba6ee9a320e604c69841bd71561af17ccd
Times <alfred.times@yahoo.fr>
2025-12-03 10:24:26 -0500

    Add menu

--- session01/src/App.jsx
+++ session01/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Menu from "./components/Menu";
 import { Header } from './components/Header'
 import { MainContent } from './components/MainContent'
 import { Footer } from './components/Footer'
@@ -12,6 +13,7 @@ function App() {
   
   return (
     <>
+       <Menu />
        <Header />
        <MainContent />
        <Footer />
